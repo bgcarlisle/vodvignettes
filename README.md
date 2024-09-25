8068fde6235c6cecfda24e126c83f459ee63a392
The research fairy <bgcarlisle@users.noreply.github.com>
2024-09-25 15:57:22 -0400

    Added README and blank config file

---
+++ README.md
@@ -0,0 +1,9 @@
+# Valley of Death Vignettes
+
+This is the expert elicitation software for the Valley of Death
+Vignettes study.
+
+## To install
+
+Make an empty MySQL database using `schema.sql` and fill in the
+`config.php` file with the appropriate credentials.
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+
+// MySQL credentials
+define('DB_USER', "");
+define('DB_PASS', "");
+define('DB_NAME', "");
+define('DB_HOST', "");
+
+// The absolute file path, include trailing slash
+define('ABS_PATH', "");
+
+// The absolute path to PHPMailer, include trailing slash
+define('PHP_MAILER_PATH', "");
+
+// The URL to the site, include leading https:// and trailing slash
+define('SITE_URL', "");
+
+// The email address that the site will send from
+define('SENDER_EMAIL', "");
+define('SENDER_USER', "");
+define('SENDER_PASS', "");
+define('SMTP_HOST', "");
+define('SMTP_PORT', "");
+
+// The name of the project (participant-facing)
+define('PROJ_NAME', "");
+
+?>
