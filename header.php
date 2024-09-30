65c96a6dcba10d42a64e29fb0f0722201713b24e
Benjamin G Carlisle PhD <bgcarlisle@users.noreply.github.com>
2024-09-30 09:02:39 -0400

    Added basic header structure

--- header.php
+++ header.php
@@ -1,3 +1,63 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	<meta charset="utf-8">
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="robots" content="noindex">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-?>
+	<title>
+	    <?php echo PROJ_NAME; ?>
+	</title>
+
+	<!-- CSS -->
+	
+	<link href="<?php echo SITE_URL; ?>/css/bootstrap.min.css" rel="stylesheet">
+
+	<link href="<?php echo SITE_URL; ?>/js/jquery-ui-1.12.1.custom/jquery-ui.css" rel="stylesheet">
+
+	<link href="<?php echo SITE_URL; ?>/css/vodvignettes.css" rel="stylesheet">
+
+    </head>
+    <body>
+	
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-bottom: 20px;">
+	    <div class="container-fluid">
+		<a class="navbar-brand" href="<?php echo SITE_URL; ?>">
+		    <img src="<?php echo SITE_URL; ?>images/logo.svg" alt="" width="17" height="24" class="d-inline-block align-top">
+		    <?php echo PROJ_NAME; ?>
+		</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarMain">
+		    <ul class="navbar-nav me-auto">
+			<li class="nav-item">
+			    <a class="nav-link active" aria-current="page" href="<?php echo SITE_URL; ?>about/">About</a>
+			</li>
+			<li class="nav-item">
+			    <a class="nav-link active" aria-current="page" href="<?php echo SITE_URL; ?>presenters/">Presenters</a>
+			</li>
+			<li class="nav-item">
+			    <a class="nav-link active" aria-current="page" href="<?php echo SITE_URL; ?>moderators/">Moderators</a>
+			</li>
+			<li class="nav-item">
+			    <a class="nav-link active" aria-current="page" href="<?php echo SITE_URL; ?>participants/">Participants</a>
+			</li>
+			<li class="nav-item">
+			    <a class="nav-link active" aria-current="page" href="<?php echo SITE_URL; ?>programme/">Programme</a>
+			</li>
+			<li class="nav-item">
+			    <a class="nav-link active" aria-current="page" href="<?php echo SITE_URL; ?>accessibility/">Accessibility</a>
+			</li>
+			<li class="nav-item">
+			    <a class="nav-link active" aria-current="page" href="<?php echo SITE_URL; ?>admin/">Admin</a>
+			</li>
+			<li class="nav-item">
+			    <a class="nav-link active" aria-current="page" href="<?php echo SITE_URL; ?>contact/">Contact</a>
+			</li>
+		    </ul>
+		</div>
+	    </div>
+	</nav>
